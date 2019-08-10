bbb6127f3d28a2df5d1235d9af5ba47d9556e49d
Alexander Kubczak <alexander.kubczak@gmail.com>
2019-08-10 16:47:40 +0200

    finishing CodeReview06

--- myscript.js
+++ myscript.js
@@ -18,76 +18,78 @@ document.getElementById("taskbutton").addEventListener("click", addTask, false);
 
 $( document ).ready(function() {
 
-$(".draggable").draggable();
-  
-$(document).on('click', '.done1', function(){
-	$(".task1").css("background", "#B5B5B5");
-	$(this).remove();
+	
+	
+	$(document).on('click', '.done1', function(){
+		$(".task1").css("background", "#B5B5B5");
+		$(this).remove();
 	});
 
-$(document).on('click', '.delete1', function(){
-	$(".task1").css("background", "#65B25F").hide(2000);
-	});
-
-
-$(document).on('click', '.done2', function(){
-	$(".task2").css("background", "#B5B5B5");
-	$(this).remove();
-	});
-
-$(document).on('click', '.delete2', function(){
-	$(".task2").css("background", "#65B25F").hide(2000);
+	$(document).on('click', '.delete1', function(){
+		$(".task1").css("background", "#65B25F").hide(2000);
 	});
 
 
-$(document).on('click', '.done3', function(){
-	$(".task3").css("background", "#B5B5B5");
-	$(this).remove();
+	$(document).on('click', '.done2', function(){
+		$(".task2").css("background", "#B5B5B5");
+		$(this).remove();
 	});
 
-$(document).on('click', '.delete3', function(){
-	$(".task3").css("background", "#65B25F").hide(2000);
-	});
-
-
-$(document).on('click', '.done4', function(){
-	$(".task4").css("background", "#B5B5B5");
-	$(this).remove();
-	});
-
-$(document).on('click', '.delete4', function(){
-	$(".task4").css("background", "#65B25F").hide(2000);
+	$(document).on('click', '.delete2', function(){
+		$(".task2").css("background", "#65B25F").hide(2000);
 	});
 
 
-$(document).on('click', '.done5', function(){
-	$(".task5").css("background", "#B5B5B5");
-	$(this).remove();
+	$(document).on('click', '.done3', function(){
+		$(".task3").css("background", "#B5B5B5");
+		$(this).remove();
 	});
 
-$(document).on('click', '.delete5', function(){
-	$(".task5").css("background", "#65B25F").hide(2000);
+	$(document).on('click', '.delete3', function(){
+		$(".task3").css("background", "#65B25F").hide(2000);
 	});
 
 
-$(document).on('click', '.done6', function(){
-	$(".task6").css("background", "#B5B5B5");
-	$(this).remove();
+	$(document).on('click', '.done4', function(){
+		$(".task4").css("background", "#B5B5B5");
+		$(this).remove();
 	});
 
-$(document).on('click', '.delete6', function(){
-	$(".task6").css("background", "#65B25F").hide(2000);
+	$(document).on('click', '.delete4', function(){
+		$(".task4").css("background", "#65B25F").hide(2000);
 	});
 
-$(document).on('click', '.done7', function(){
-    $('.task7').css("background", "#B5B5B5");
-    $(this).remove();
-});
+
+	$(document).on('click', '.done5', function(){
+		$(".task5").css("background", "#B5B5B5");
+		$(this).remove();
+	});
+
+	$(document).on('click', '.delete5', function(){
+		$(".task5").css("background", "#65B25F").hide(2000);
+	});
 
 
-$(document).on('click', '.delete7', function(){
-    $(".task7").css("background", "#65B25F").hide(2000);
-});
+	$(document).on('click', '.done6', function(){
+		$(".task6").css("background", "#B5B5B5");
+		$(this).remove();
+	});
 
+	$(document).on('click', '.delete6', function(){
+		$(".task6").css("background", "#65B25F").hide(2000);
+	});
+
+	$(document).on('click', '.done7', function(){
+		$('.task7').css("background", "#B5B5B5");
+		$(this).remove();
+	});
+
+
+	$(document).on('click', '.delete7', function(){
+		$(".task7").css("background", "#65B25F").hide(2000);
+	});
+
+
+	$(".draggable").draggable();
 
 });
